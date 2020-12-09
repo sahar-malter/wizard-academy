cb47ccca29e4748750918e434dd13f68fc3028a6
sahar <sahar.malter1@gmail.com>
2020-12-09 11:41:01 +0200

    added terrain, falling cieling +particles, camera shake - trigger

---
+++ Assets/first_reveal.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class first_reveal : MonoBehaviour
+{
+    public List<GameObject> cieling_disappear;
+    public ParticleSystem falling_rocks;
+    public Animator cam_anim;
+    private bool has_shaked;
+    // Start is called before the first frame update
+    void Start()
+    {
+        has_shaked = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!has_shaked && other.CompareTag("Player"))
+        {
+            has_shaked = true;
+            falling_rocks.Play();
+            cam_anim.SetTrigger("shake");
+            for (int i = 0; i < cieling_disappear.Count; i++)
+            {
+                cieling_disappear[i].SetActive(false);
+
+            }
+
+        }
+    }
+
+}
